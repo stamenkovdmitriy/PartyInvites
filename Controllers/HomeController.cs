2e4b1f465bba15de78dc85abcce52f4fda85a235
user <user@WIN-T8D95CG425I>
2023-02-19 08:17:35 +0300

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PartyInvites.Models;
+using System.Diagnostics;
+using PartyInvites.Models;
+using System.Linq;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        // метод действия который выбирает стандартное представление подлежащее визуализации
+        public IActionResult Index() 
+        {
+            return View();    
+        }
+
+        //  метод действия Rsvp отвечает за отображения изначальной пустой формы
+        [HttpGet]  // атрибут объявляющий чем будет заниматься метод
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        // метод Rsvp отвечающий за получение отправленных данных и принятие решения что с ними делать
+        [HttpPost]  // атрибут объявляющий чем будет заниматься метод
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            // что сделать - сохранить ответ от гостя
+            if (ModelState.IsValid)   // свойство  проверяющее достоверность данных
+            {
+                Repository.AddResponse(guestResponse);
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        // метод действия добавляющий конечную точку для ListResponses
+        public ViewResult ListResponses()
+        {
+            return View(Repository.Responses.Where(r => r.WillAttend == true));  // с помощью Linq получать положительные ответы
+        }
+
+    }
+}
---
+++ Models/GuestResponse.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PartyInvites.Models
+{
+    // класс подверждающий ответ наприглашение
+    public class GuestResponse
+    {
+        [Required(ErrorMessage ="Please enter your name")]
+        // Пожалуйста введите свое имя
+        public string Name { get; set; }
+
+        [Required(ErrorMessage ="Please enter your eemail address")]
+        [EmailAddress]
+        // Пожалуйста введите адре электронной почты
+        public string Email { get; set; }
+
+        [Required(ErrorMessage ="Pleaseenter your phone number")]
+        // Пожалуйста введите номер телефона
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage ="Please specify whether you'll attend")]
+        // Пожалуйста укажите примите ли вы участие
+        public bool? WillAttend { get; set; } 
+    }
+}
